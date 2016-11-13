f8b3e2173b58bcbda436988be0520abf4996eef1
Agustin Houlgrave <a.houlgrave@gmail.com>
2016-11-13 23:01:52 +0100

    example fix

--- examples/sample.php
+++ examples/sample.php
@@ -1,4 +1,5 @@
 <?php
+use Silex\Application;
 use Telegram\Bot\Silex\Command\ApplicationAwareCommand;
 use Telegram\Bot\Silex\Provider\TelegramControllerProvider;
 use Telegram\Bot\Silex\Provider\TelegramServiceProvider;
@@ -7,7 +8,14 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 class MyCommand extends ApplicationAwareCommand
 {
+    /**
+     * @inheritdoc
+     */
     protected $name = 'hello';
+
+    /**
+     * @inheritdoc
+     */
     public function handle($arguments)
     {
         $app = $this->getApplication();
@@ -15,7 +23,7 @@ class MyCommand extends ApplicationAwareCommand
     }
 }
 
-$app = new \Silex\Application();
+$app = new Application();
 $app->register(new TelegramServiceProvider(), [
     'telegram.bot_api' => '<test>',
     'telegram.commands' => [
